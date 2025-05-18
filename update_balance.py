01c2693c0b4e861294407e39ff68631fa9ecd77c
BAIkng4 <141310241+BAIkng4@users.noreply.github.com>
2025-05-19 00:16:25 +0700

    Update update_balance.py

--- update_balance.py
+++ update_balance.py
@@ -1,26 +1,93 @@
+import requests, json
+from datetime import datetime, timezone
+
+# Read config.json
+with open("config.json", "r") as file:
+    config = json.load(file)
+    
 class BalanceUpdater:
-    def update_balance(self, collection, username, amount):
+    def get_user_profile(self):
+        user_url = "https://discord.com/api/v9/users/@me"
+        headers = {
+            "Authorization": f"Bot {config['discord']['discord_token']}"
+        }
+
+        response = requests.get(user_url, headers=headers)
+        if response.status_code == 200:
+            user_data = response.json()
+            avatar = f'https://cdn.discordapp.com/avatars/{user_data["id"]}/{user_data["avatar"]}.png'
+            return avatar
+        else:
+            print("Failed to fetch Discord user data:", response.status_code, response.text)
+            return ""
+
+    def get_dm_channel_id(self, userid):
+        channel_url = "https://discord.com/api/v9/users/@me/channels"
+        headers = {
+            "Authorization": f"Bot {config['discord']['discord_token']}",
+            "Content-Type": "application/json"
+        }
+        json_data = {"recipient_id": userid}
+
+        response = requests.post(channel_url, headers=headers, json=json_data)
+        if response.status_code == 200:
+            return response.json()["id"]
+        else:
+            print("Failed to fetch Discord channel id:", response.status_code, response.text)
+            return ""
+
+    def send_message_discord(self, userid, donator_name, previous_balance, balance_change, new_balance):
+        avatar = self.get_user_profile()
+        dm_channel_id = self.get_dm_channel_id(userid)
+
+        embed = {
+            "title": f"{donator_name}",
+            "description": "Thank you for topping up your balance.",
+            "color": int("3bff5f", 16),
+            "fields": [
+                {"name": "Previous Balance", "value": f"{previous_balance}", "inline": False},
+                {"name": "Balance Change", "value": f"+{balance_change}", "inline": False},
+                {"name": "Current Balance", "value": f"{new_balance}", "inline": False}
+            ],
+            "footer": {"text": "Grow King Bot", "icon_url": avatar},
+            "timestamp": datetime.utcnow().replace(tzinfo=timezone.utc).isoformat()
+        }
+
+        headers = {
+            "Authorization": f"Bot {config['discord']['discord_token']}",
+            "Content-Type": "application/json"
+        }
+
+        data = {"embeds": [embed]}
+        logs_url = f"https://discord.com/api/v9/channels/{config['channel_id_logs']['balance_logs']}/messages"
+        requests.post(logs_url, headers=headers, data=json.dumps(data))
+
+        # DM user
+        if dm_channel_id:
+            dm_url = f"https://discord.com/api/v9/channels/{dm_channel_id}/messages"
+            requests.post(dm_url, headers=headers, data=json.dumps(data))
+
+    def update_balance(self, mongodb_user_collection, backup_db, backup_folder_user, donator_name, amount):
         try:
-            user = collection.find_one({"username": username})
+            user_data = mongodb_user_collection.find_one({"donator_name": donator_name})
+            previous_balance = user_data.get("balance", 0) if user_data else 0
+            new_balance = previous_balance + amount
             
-            if not user:
-                print(f"User {username} tidak ditemukan.")
-                return False
+            if user_data:
+                userid = user_data.get('userid', '0')
+                mongodb_user_collection.update_one({"donator_name": donator_name}, {"$set": {"balance": new_balance}})
 
-            current_balance = user.get("balance", 0)
-            new_balance = current_balance + amount
+                # Delete old backup
+                old_filename = f"{userid}_{user_data.get('code', 0)}"
+                if backup_db.read_data(backup_folder_user, old_filename):
+                    backup_db.delete_data(backup_folder_user, old_filename)
 
-            result = collection.update_one(
-                {"username": username},
-                {"$set": {"balance": new_balance}}
-            )
+                # Upload new backup
+                backup_user_data = mongodb_user_collection.find_one({"donator_name": donator_name})
+                new_filename = f"{backup_user_data.get('userid', '0')}_{backup_user_data.get('code', 0)}"
+                backup_db.update_data(backup_folder_user, new_filename, backup_user_data)
 
-            if result.modified_count == 1:
-                print(f"Saldo {username} berhasil diupdate menjadi {new_balance}.")
-            else:
-                print(f"Saldo {username} tidak berubah.")
-            return True
-
+                self.send_message_discord(userid, donator_name, previous_balance, amount, new_balance)
         except Exception as e:
-            print(f"Error saat update saldo: {e}")
-            return False
+            print(f"[ERROR] Failed to update balance: {e}")
+
