974deb3950fd62274208b73fdf14a7105bb368b5
BAIkng4 <141310241+BAIkng4@users.noreply.github.com>
2025-05-18 22:49:14 +0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,8 +1,26 @@
-from flask import Flask
+from flask import Flask, jsonify
+from pymongo import MongoClient
+import os
 
 app = Flask(__name__)
 
+# Ambil URL MongoDB dari environment variable (Railway menyediakannya)
+mongo_uri = os.getenv("mongodb+srv://gksbot:gttopia01@datacluster.cn2geam.mongodb.net/")  # variabel ini kamu isi di Railway
+client = MongoClient(mongo_uri)
+
+# Ganti sesuai nama database dan koleksi kamu
+db = client["gks_bot"]
+collection = db["user_data"]
 
 @app.route('/')
 def hello():
-    return 'Hello world, welcome to Railway!'
+    # Ambil data dari koleksi MongoDB
+    documents = collection.find()
+    
+    # Ubah ke bentuk list agar bisa ditampilkan
+    result = []
+    for doc in documents:
+        doc["_id"] = str(doc["_id"])  # ubah ObjectId jadi string agar JSON-valid
+        result.append(doc)
+
+    return jsonify(result)
